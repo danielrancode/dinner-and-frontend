62ad07c19c1b52cc2bfc41bad1e8e7b28dd652fe
Daniel Ran <danielr1735@gmail.com>
2018-09-12 12:11:04 -0400

    added funcationality

--- src/containers/ResultsContainer.js
+++ src/containers/ResultsContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import locationData from '../locationData.js'
 import ResultsGrid from '../components/ResultsGrid.js'
 import Search from '../components/Search'
+import { Button } from 'semantic-ui-react'
 
 
 class ResultsContainer extends Component {
@@ -32,8 +33,8 @@ class ResultsContainer extends Component {
   ////////// state methods
 
   getData(zip, lat, lon) {
-    fetch(`http://localhost:4000/api/v1/restaurants?location=${zip}`).then(r => r.json()).then(data => this.setState({restaurants: data.businesses}))
-    fetch(`http://localhost:4000/api/v1/events?lat=${lat}&lon=${lon}`).then(r => r.json()).then(data => this.setState({events: data.events}))
+    fetch(`http://localhost:4000/api/v1/restaurants/search?location=${zip}`).then(r => r.json()).then(data => this.setState({restaurants: data.businesses}))
+    fetch(`http://localhost:4000/api/v1/events/search?lat=${lat}&lon=${lon}`).then(r => r.json()).then(data => this.setState({events: data.events}))
   }
 
   setSearchTerm(term) {
@@ -44,18 +45,23 @@ class ResultsContainer extends Component {
   }
 
   saveRestaurant(restObj) {
-    console.log("saveRestaurant, ", restObj)
+    this.setState({selectedRestaurant: restObj})
   }
 
   saveEvent(eventObj) {
-    console.log("saveEvent, ", eventObj)
+    this.setState({selectedEvent: eventObj})
   }
 
+  saveProgram() {
+    console.log("restaurant", this.state.selectedRestaurant)
+    console.log("event", this.state.selectedEvent)
+  }
 
   render() {
     return (
       <div>
         <Search handleChange={this.setSearchTerm.bind(this)}/>
+        <Button onClick={this.saveProgram.bind(this)}>Save program</Button>
         <ResultsGrid saveRestaurant={this.saveRestaurant.bind(this)} saveEvent={this.saveEvent.bind(this)} events={this.state.events} restaurants={this.state.restaurants}/>
       </div>
     )
